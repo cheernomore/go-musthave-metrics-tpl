cb402c1f8bcd7796bf6411c93d42c5b172b0d637
ch33rnomore <ch33rnomore@gmail.com>
2026-04-09 21:46:27 +0300

    fix: add migrations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,9 @@ import (
 	"github.com/cheernomore/go-musthave-metrics-tpl/internal/logger"
 	"github.com/cheernomore/go-musthave-metrics-tpl/internal/repository"
 	"github.com/go-chi/chi/v5"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"go.uber.org/zap"
 	"net/http"
@@ -32,7 +35,6 @@ func run() error {
 
 	var repo repository.MetricsRepository
 
-	// Приоритет 1: PostgreSQL (если DATABASE_DSN указан и доступен)
 	if cfg.DatabaseDSN != "" {
 		logger.Log.Info("--- start connection to db")
 		var err error
@@ -49,20 +51,25 @@ func run() error {
 				db = nil
 			} else {
 				defer db.Close()
-				logger.Log.Info("--- успешное подключение к БД, используем PostgreSQL для хранения метрик")
-				repo = repository.NewPostgresRepository(db)
+				logger.Log.Info("--- успешное подключение к БД")
+
+				if err := runMigrations(db); err != nil {
+					logger.Log.Warn("не удалось применить миграции, fallback на файловое хранилище", zap.Error(err))
+					db.Close()
+					db = nil
+				} else {
+					logger.Log.Info("используем PostgreSQL для хранения метрик")
+					repo = repository.NewPostgresRepository(db)
+				}
 			}
 		}
 	} else {
 		logger.Log.Info("DATABASE_DSN не указан")
 	}
 
-	// Приоритет 2: Файловое хранилище (если БД недоступна и FileStoragePath указан)
-	// Приоритет 3: Память (если ничего не указано)
 	if repo == nil {
 		memStorage := repository.NewMemStorage()
 
-		// Восстанавливаем данные из файла при старте
 		if cfg.Restore && cfg.FileStoragePath != "" {
 			if err := memStorage.LoadFromFile(cfg.FileStoragePath); err != nil {
 				logger.Log.Warn("не удалось загрузить данные из файла", zap.Error(err))
@@ -73,13 +80,11 @@ func run() error {
 
 		repo = memStorage
 
-		// Если указан путь к файлу и StoreInterval == 0 -> синхронное сохранение
 		if cfg.FileStoragePath != "" && cfg.StoreInterval == 0 {
 			logger.Log.Info("используем синхронное сохранение в файл")
 			repo = repository.NewFileBackedRepository(memStorage, cfg.FileStoragePath)
 		}
 
-		// Если указан путь к файлу и StoreInterval > 0 -> периодическое сохранение
 		if cfg.FileStoragePath != "" && cfg.StoreInterval > 0 {
 			logger.Log.Info("используем периодическое сохранение в файл", zap.Int("interval", cfg.StoreInterval))
 			go func() {
@@ -116,6 +121,24 @@ func run() error {
 
 	logger.Log.Info("Running server", zap.String("address", cfg.Address))
 	return http.ListenAndServe(cfg.Address, r)
+}
+
+func runMigrations(db *sql.DB) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance("file://migrations", "postgres", driver)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
